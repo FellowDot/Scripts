e218eef54be8dc0aaab710a5ca4b8c6450ad9f05
FellowDot <90349657+FellowDot@users.noreply.github.com>
2021-09-09 17:43:36 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 (function(){
     
-    $('body').append("<p>Vai do Fellow-e pra Nuvem</p>");
+    $('body').append("<p>A Fellow ecossystem.</p>");
 
     // PEGA O PARÂMETRO QUE VAI NA TAG <SCRIPT
     // var one = document.currentScript.getAttribute('one'); //1
     // alert(one);
 
-    var reply_click = function(){
-        var email = document.getElementById('fellow-email').value
-        var pwd = document.getElementById('fellow-password').value
-        alert("Tentativa de login:\nEmail:" + email + "\nPassword:" + pwd);
-    }
+    //var reply_click = function(){
+        //var email = document.getElementById('fellow-email').value
+        //var pwd = document.getElementById('fellow-password').value
+        //alert("Tentativa de login:\nEmail:" + email + "\nPassword:" + pwd);
+    //}
 
-    document.getElementById('fellow-btn').onclick = reply_click;
+    //document.getElementById('fellow-btn').onclick = reply_click;
 })();

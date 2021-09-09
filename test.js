88d608b49bf806f6ff38d1558eb9ea4db85a923d
FellowDot <90349657+FellowDot@users.noreply.github.com>
2021-09-09 16:16:03 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
-$('body').append("<p>Vai do Fellow-e pra Nuvem</p>");
+(function(){
+    
+    $('body').append("<p>Vai do Fellow-e pra Nuvem</p>");
 
-// PEGA O PARÂMETRO QUE VAI NA TAG <SCRIPT
-// var one = document.currentScript.getAttribute('one'); //1
-// alert(one);
+    // PEGA O PARÂMETRO QUE VAI NA TAG <SCRIPT
+    // var one = document.currentScript.getAttribute('one'); //1
+    // alert(one);
 
-var reply_click = function(){
-    var email = document.getElementById('fellow-email').value
-    var pwd = document.getElementById('fellow-password').value
-    alert("Tentativa de login:\nEmail:" + email + "\nPassword:" + pwd);
-}
+    var reply_click = function(){
+        var email = document.getElementById('fellow-email').value
+        var pwd = document.getElementById('fellow-password').value
+        alert("Tentativa de login:\nEmail:" + email + "\nPassword:" + pwd);
+    }
 
-document.getElementById('fellow-btn').onclick = reply_click;
+    document.getElementById('fellow-btn').onclick = reply_click;
+})();

742a88b1ed10caf764a6b13e403bffeced58f8db
João Marcos <joao.freitas@fellowdot.com>
2021-09-10 17:20:55 -0300

    TEste 06

--- nuvem.js
+++ nuvem.js
@@ -11,21 +11,48 @@ var loadScript = function(url, callback){
        $ in this scope references the jQuery object we'll use.
        Don't use 'jQuery', or 'jQuery191', here. Use the dollar sign
        that was passed as argument.*/
-    $('body').append("<p>IGostei de Duloc, bem limpa "+$.fn.jquery+'</p>');
+    // $('__next').append("<p>Gostei de Duloc, bem limpa "+$.fn.jquery+'</p>');
+    var next = document.getElementById('__next');
+    next.append("Gostei de Duloc, muito limpa "+$.fn.jquery);
   };
-  
-  // For jQuery version 1.7
-  var target = [1, 7, 0];
-  
-  var current = typeof jQuery === 'undefined' ? [0,0,0] : $.fn.jquery.split('.').map(function(item) {
-      return parseInt(item);
-  });
-  
-  if (current[0] < target[0] || (current[0] == target[0] && current[1] < target[1])) {
+
+var scripts = document.getElementsByTagName('script');
+var myScript = scripts[ scripts.length - 1 ];
+
+var queryString = myScript.src.replace(/^[^\?]+\??/,'');
+
+var params = parseQuery( queryString );
+
+function parseQuery ( query ) {
+    var Params = new Object ();
+    if ( ! query ) return Params; // return empty object
+    
+    var Pairs = query.split(/[;&]/);
+    for ( var i = 0; i < Pairs.length; i++ ) {
+        var KeyVal = Pairs[i].split('=');
+        if ( ! KeyVal || KeyVal.length != 2 ) continue;
+        var key = unescape( KeyVal[0] );
+        var val = unescape( KeyVal[1] );
+        val = val.replace(/\+/g, ' ');
+        Params[key] = val;
+    }
+    return Params;
+}
+
+console.log("store = ", params["store"])
+
+// For jQuery version 1.7
+var target = [1, 7, 0];
+
+var current = typeof jQuery === 'undefined' ? [0,0,0] : $.fn.jquery.split('.').map(function(item) {
+    return parseInt(item);
+});
+
+if (current[0] < target[0] || (current[0] == target[0] && current[1] < target[1])) {
     loadScript('//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js', function(){
-      var jQuery1101 = jQuery.noConflict(true);
-      myAppJavaScript(jQuery1101);
+        var jQuery1101 = jQuery.noConflict(true);
+        myAppJavaScript(jQuery1101);
     });
-  } else {
+} else {
     myAppJavaScript(jQuery);
-  }
+}

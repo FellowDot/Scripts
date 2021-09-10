c26f6518c98a6734ad28fd8ed0ae53268f65fc85
João Marcos <joao.freitas@fellowdot.com>
2021-09-10 14:16:15 -0300

    Teste 02

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 (function(){
     
+    $('body').append("<p>Os ogros tem camadas, as cebolas tem camadas.</p>");
     $('body').append("<p>É por isso que ninguém gosta de ogros.</p>");
     $('body').append("<p>Gostei de Duloc, bem limpa.</p>");
 

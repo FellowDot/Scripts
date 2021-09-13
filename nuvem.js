15e095076b4580faf04579f5bf5f0625d1afb807
João Marcos <joao.freitas@fellowdot.com>
2021-09-13 15:16:07 -0300

    TEste 10

--- nuvem.js
+++ nuvem.js
@@ -4,20 +4,20 @@ var loadScript = function(url, callback){
        We recommend this code: https://snipplr.com/view/18756/loadscript/.
        Once the jQuery library is loaded, the callback function will be executed. */
   
-  };
+};
   
-  var myAppJavaScript = function($){
+var myAppJavaScript = function($){
     /* Your app's JavaScript here.
        $ in this scope references the jQuery object we'll use.
        Don't use 'jQuery', or 'jQuery191', here. Use the dollar sign
        that was passed as argument.*/
 
     console.log("ENtrou na função");
-    console.log($('form'));
-    // $('form').append("<p>Gostei de Duloc, bem limpa "+$.fn.jquery+'</p>');
+    console.log($('body'));
+    $('form').append("<p>Gostei de Duloc, bem limpa "+$.fn.jquery+'</p>');
     // var next = document.getElementById('__next');
     // next.append("Gostei de Duloc, muito limpa "+$.fn.jquery);
-  };
+};
 
 var scripts = document.getElementsByTagName('script');
 var myScript = scripts[ scripts.length - 1 ];
@@ -44,18 +44,12 @@ function parseQuery ( query ) {
 
 console.log("store = ", params["store"])
 
-// For jQuery version 1.7
-var target = [1, 7, 0];
+// var reply_click = function(){
+//     var email = document.getElementById('fellow-email').value
+//     var pwd = document.getElementById('fellow-password').value
+//     $('form').append("<p>Gostei de Duloc, bem limpa "+$.fn.jquery+'</p>');
+// }
 
-var current = typeof jQuery === 'undefined' ? [0,0,0] : $.fn.jquery.split('.').map(function(item) {
-    return parseInt(item);
-});
+// document.getElementById('fellow-btn').onclick = reply_click;
 
-if (current[0] < target[0] || (current[0] == target[0] && current[1] < target[1])) {
-    loadScript('//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js', function(){
-        var jQuery1101 = jQuery.noConflict(true);
-        myAppJavaScript(jQuery1101);
-    });
-} else {
-    myAppJavaScript(jQuery);
-}
+myAppJavaScript(jQuery);

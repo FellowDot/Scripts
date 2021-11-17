c64415b0e25499bc14721630dd1878e399884f82
FellowDot <90349657+FellowDot@users.noreply.github.com>
2021-11-17 17:28:34 -0300

    Update nuvem.js

--- nuvem.js
+++ nuvem.js
@@ -207,6 +207,7 @@ var fillModal = function(modal_content){
         fellow_i_frame.title = "Fellow-e";
         fellow_i_frame.style.width = "100%"
         fellow_i_frame.style.height = "80%"
+        fellow_i_frame.scrolling = "no"
         modal_content.appendChild(fellow_i_frame);
     }
     else{
